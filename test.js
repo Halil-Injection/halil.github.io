57c1d710f7152cffe1c334f0602aa30bf3d0f39b
Halil <73217512+Halil-Injection@users.noreply.github.com>
2020-10-21 09:57:51 +0200

    Add files via upload

---
+++ test.js
@@ -0,0 +1,7 @@
+function openxD() {
+    alert("");
+    alert("Du wirst zu den FlameVace-News weitergeleitet!");
+    setTimeout(function() {
+        window.location.assign("https://news.flamevace.eu");
+    }, 2500);
+}
